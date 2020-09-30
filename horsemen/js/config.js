2525722a9a55793d3cd33c5917c886ab30457d13
lawlighty <978170580@qq.com>
2020-09-30 13:30:41 +0800

    上线线区分

--- horsemen/js/config.js
+++ horsemen/js/config.js
@@ -1,5 +1,6 @@
 
 function getUrl() {
-    fetchPrefix = 'https://dev.ppxian.cn/'
+    fetchPrefix = 'https://dev.ppxian.cn/' //测试
+    // fetchPrefix = 'http://192.168.10.22:8011/' //
     return fetchPrefix
 }   
